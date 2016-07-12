b57a4faad31b46576a351772927d34e85c88ec3d
fury2003 <udvhoangtrung@gmail.com>
2016-07-13 00:19:42 +0700

    UPdate
    thay dổi vớ vẩn

--- src/ui/FrQuestion.java
+++ src/ui/FrQuestion.java
@@ -6,6 +6,7 @@
 package ui;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
@@ -16,6 +17,7 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.effect.DropShadow;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
@@ -37,7 +39,6 @@ public class FrQuestion{
     private TableView table = new TableView();
     public FrQuestion(HBox root) {
         GridPane gridPane = new GridPane();
-        System.out.println(root.getWidth());
         gridPane.setAlignment(Pos.CENTER_LEFT);
         gridPane.setHgap(20);
         gridPane.setVgap(10);
@@ -67,13 +68,14 @@ public class FrQuestion{
         );
         cbbCategory.getSelectionModel().selectFirst();
         gridPane.add(cbbCategory, 1, 1);
-        
-        TextArea txtQuestion = new TextArea();
-        
+        TextArea txtQuestion = new TextArea();        
         txtQuestion.setPrefSize(500, 150);
         gridPane.add(txtQuestion, 1, 2);
         
+        //button search
         VBox vbBox = new VBox(10);
+        Label lb = new Label("");
+        Label lb1 = new Label("");
         
         Button btnViewAll = new Button("Xem tất cả");        
         HBox.setHgrow(btnViewAll, Priority.ALWAYS); 
@@ -86,12 +88,12 @@ public class FrQuestion{
         btnSearchAll.setMaxWidth(Double.MAX_VALUE);
         btnSearchAll.getStyleClass().add("btnNor");
         btnSearchAll.getStylesheets().add(frameOpenGame.class.getResource("/css/frameOpenGame.css").toExternalForm());
-
-        
-        vbBox.getChildren().addAll(btnViewAll,btnSearchAll);
-        
+   
+        vbBox.getChildren().addAll(lb,lb1,btnViewAll,btnSearchAll);     
         gridPane.add(vbBox, 2, 2, 1, 3);
         
+        //table
+        table.setPrefSize(600, 300);
         TableColumn QuesID = new TableColumn("Question ID");
         TableColumn QuesContent = new TableColumn("Question Content");
         TableColumn CateID = new TableColumn("Category ID");    
@@ -103,25 +105,51 @@ public class FrQuestion{
 
         final VBox vbox = new VBox();
         vbox.setSpacing(5);
-        vbox.getChildren().addAll(table);
-        
+        vbox.getChildren().addAll(table);    
         gridPane.add(vbox, 1, 3);
-        
-        VBox hbbtnQuestion = new VBox(10);
+        // button list
+        VBox vbbtn = new VBox(10);
+        vbbtn.setPrefWidth(100);
+        Label lb2 = new Label("");
+        Label lb3 = new Label("");
+        Label lb4 = new Label("");
         Button btnAdd = new Button("Thêm");
         Button btnUpdate = new Button("Cập nhật");
+        Button btnDelete = new Button("Xóa");
+        Button btnCancel = new Button("Thoát");
+        
         HBox.setHgrow(btnAdd, Priority.ALWAYS);
         btnAdd.setMaxWidth(Double.MAX_VALUE);
         btnAdd.getStyleClass().add("btnNor");
         btnAdd.getStylesheets().add(frameOpenGame.class.getResource("/css/frameOpenGame.css").toExternalForm());
+        
         HBox.setHgrow(btnUpdate, Priority.ALWAYS);
         btnUpdate.setMaxWidth(Double.MAX_VALUE);
         btnUpdate.getStyleClass().add("btnNor");
         btnUpdate.getStylesheets().add(frameOpenGame.class.getResource("/css/frameOpenGame.css").toExternalForm());
-        hbbtnQuestion.getChildren().addAll(btnAdd,btnUpdate);
-        gridPane.add(hbbtnQuestion, 2, 3);
+        
+        HBox.setHgrow(btnUpdate, Priority.ALWAYS);
+        btnDelete.setMaxWidth(Double.MAX_VALUE);
+        btnDelete.getStyleClass().add("btnNor");
+        btnDelete.getStylesheets().add(frameOpenGame.class.getResource("/css/frameOpenGame.css").toExternalForm());
+        
+        HBox.setHgrow(btnUpdate, Priority.ALWAYS);
+        btnCancel.setMaxWidth(Double.MAX_VALUE);
+        btnCancel.getStyleClass().add("btnNor");
+        btnCancel.getStylesheets().add(frameOpenGame.class.getResource("/css/frameOpenGame.css").toExternalForm());
+        
+        vbbtn.getChildren().addAll(lb2,lb3,lb4,btnAdd,btnUpdate,btnDelete,btnCancel);
+        gridPane.add(vbbtn, 2, 3);
         
         root.getChildren().add(gridPane);
+        
+        btnCancel.setOnMouseClicked(new EventHandler<MouseEvent>(){
+            @Override
+            public void handle(MouseEvent event) {
+                new FrCheckQuestion();
+            }
+            
+        });
     }
 
 
