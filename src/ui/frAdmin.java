d8b6e979ad569727d164b3eb61e316706bd6b370
fury2003 <udvhoangtrung@gmail.com>
2016-07-13 00:16:35 +0700

    Admin pane

--- src/ui/frAdmin.java
+++ src/ui/frAdmin.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
@@ -20,13 +19,11 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
-import javax.swing.text.AbstractDocument;
 import lib.openSound;
 
 
